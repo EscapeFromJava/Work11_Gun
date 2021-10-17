668cdbfa572f6f8bc605684cb9dc22162d3c6577
EscapeFromJava <reske.artur@gmail.com>
2021-10-17 23:08:10 +0300

    Добавьте файлы проекта.

---
+++ Work11_Gun/Source.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	double x, y, R, result;
+	cout << "Введите параметры R, x, y: " << endl;
+	cin >> R >> x >> y;
+	if (x < -10 || x > 10 || y < 0 || y >= R || R > 10) {
+		cout << "Параметры введены неверно" << endl;
+		return 0;
+	}
+	result = x / 3 * 2;
+	cout << "Расстояние по прямой линии от оси OY до точки удара шарика о стену: " << result << endl;
+	return 0;
+}
