d178acf4634cd1ec8fd170e9ec366feaf50f84a1
EscapeFromJava <reske.artur@gmail.com>
2021-10-18 11:05:58 +0300

    v 1.1

--- Work11_Gun/Source.cpp
+++ Work11_Gun/Source.cpp
@@ -4,14 +4,15 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "rus");
-	double x, y, R, result;
+	double x, y, R, k, result;
 	cout << "Введите параметры R, x, y: " << endl;
 	cin >> R >> x >> y;
 	if (x < -10 || x > 10 || y < 0 || y >= R || R > 10) {
 		cout << "Параметры введены неверно" << endl;
 		return 0;
 	}
-	result = x / 3 * 2;
+	k = R / (R - y); //коэффициент подобия
+	result = k * (x / (k + 1));
 	cout << "Расстояние по прямой линии от оси OY до точки удара шарика о стену: " << result << endl;
 	return 0;
 }
